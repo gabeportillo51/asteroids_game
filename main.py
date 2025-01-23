40f652be8ddd00da7f393a7108b01e47afde6d77
gabeportillo51 <gaporti51@gmail.com>
2025-01-23 09:28:42 -0700

    Blank window working

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame                # allows use of pygame's modules
+from constants import *   # imports variables from constants.py
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while pygame.get_init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
